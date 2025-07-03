72cde38959ab812ddcd36f3f4265327ad2b9c84e
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-07-03 17:53:57 +0700

    feat: extend CORS configuration to include additional production URL

--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 3001;
 
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://sybershop.vercel.app'],
+        origin: [
+            'http://localhost:5173',
+            'https://sybershop.vercel.app',
+            'https://sybershop-tatuanthanhs-projects.vercel.app/',
+        ],
         credentials: true,
     }),
 );
